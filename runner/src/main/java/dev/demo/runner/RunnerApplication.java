7e329708cb1e8efc5a49cb1babd9447c2dd7032e
Parimi <parimiayyappa@gmail.com>
2025-11-04 15:04:26 -0600

    WorkingWithDatabase1

--- runner/src/main/java/dev/demo/runner/RunnerApplication.java
+++ runner/src/main/java/dev/demo/runner/RunnerApplication.java
@@ -1,18 +1,9 @@
 package dev.demo.runner;
 
-import dev.demo.runner.run.Location;
-import dev.demo.runner.run.Run;
-import dev.demo.runner.run.RunRepository;
-import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class RunnerApplication {
@@ -25,12 +16,12 @@ public class RunnerApplication {
 
 	}
 
-	@Bean
-	CommandLineRunner runner(RunRepository runRepository) {
-		return args -> {
-			Run run =  new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
-			runRepository.create(run);
-		};
-	}
+//	@Beans
+//	CommandLineRunner runner(RunRepository runRepository) {
+//		return args -> {
+//			Run run =  new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+//			runRepository.create(run);
+//		};
+//	}
 
 }
