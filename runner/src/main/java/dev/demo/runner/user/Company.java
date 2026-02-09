d6abcd864475849423f988a39d5507ebe7c5b947
Parimi <parimiayyappa@gmail.com>
2026-02-09 04:35:31 -0600

    Rest Clients

---
+++ runner/src/main/java/dev/demo/runner/user/Company.java
@@ -0,0 +1,8 @@
+package dev.demo.runner.user;
+
+public record Company(
+        String name,
+        String catchPhrase,
+        String bs
+) {
+}
---
+++ runner/src/main/java/dev/demo/runner/user/Geo.java
@@ -0,0 +1,7 @@
+package dev.demo.runner.user;
+
+public record Geo(
+        String lat,
+        String lng
+) {
+}
